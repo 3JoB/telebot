ef12f8f5caf29d33983e1ff8dbcdfaab2fa1f7b6
Zero <54469706+3JoB@users.noreply.github.com>
2023-09-06 17:00:04 +0800

    add

--- internal/net/fasthttp.go
+++ internal/net/fasthttp.go
@@ -1,1 +1,20 @@
 package net
+
+import (
+	"github.com/valyala/fasthttp"
+	"github.com/valyala/fasthttp/fasthttpproxy"
+)
+
+type FastHTTP struct {
+	client *fasthttp.Client
+}
+
+func NewFastHTTPClient() {
+	_ = &FastHTTP{
+		client: &fasthttp.Client{
+			NoDefaultUserAgentHeader:      true,
+			DisableHeaderNamesNormalizing: false,
+			Dial:                          fasthttpproxy.FasthttpProxyHTTPDialer(),
+		},
+	}
+}
--- internal/net/net.go
+++ internal/net/net.go
@@ -1,3 +1,16 @@
 package net
 
-type NetFrame any
+import "github.com/3JoB/telebot/internal/json"
+
+type NetFrame interface{
+	SetClient()
+	SetJson(v json.Json)
+	GETFile()
+	POSTFile()
+	GETJson()
+	POSTJson()
+}
+
+var header = map[string]string{
+
+}
