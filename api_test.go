6d233b15a29d3dcb5f89db12878034e8e099f551
Zero <54469706+3JoB@users.noreply.github.com>
2023-09-12 23:24:05 +0800

    add && remove

--- api_test.go
+++ api_test.go
@@ -1,50 +1,12 @@
 package telebot
 
 import (
-	"errors"
-	"net/http"
-	"strings"
 	"testing"
 
 	"github.com/3JoB/unsafeConvert"
-	"github.com/goccy/go-json"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
-
-// testPayload implements json.Marshaler
-// to test json encoding error behaviour.
-type testPayload struct{}
-
-func (testPayload) MarshalJSON() ([]byte, error) {
-	return nil, errors.New("test error")
-}
-
-func testRawServer(w http.ResponseWriter, r *http.Request) {
-	switch {
-	// causes EOF error on ioutil.ReadAll
-	case strings.HasSuffix(r.URL.Path, "/testReadError"):
-		// tells the body is 1 byte length but actually it's 0
-		w.Header().Set("Content-Length", "1")
-
-	// returns unknown telegram error
-	case strings.HasSuffix(r.URL.Path, "/testUnknownError"):
-		data, _ := json.Marshal(struct {
-			Ok          bool   `json:"ok"`
-			Code        int    `json:"error_code"`
-			Description string `json:"description"`
-		}{
-			Ok:          false,
-			Code:        400,
-			Description: "unknown error",
-		})
-
-		w.WriteHeader(400)
-		w.Write(data)
-	}
-}
-
-func TestRaw(t *testing.T) {}
 
 func TestExtractOk(t *testing.T) {
 	data := unsafeConvert.ByteSlice(`{"ok": true, "result": {}}`)
