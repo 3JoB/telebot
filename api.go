3c12ccf49221249144fb836edef16b498e75b002
Zero <54469706+3JoB@users.noreply.github.com>
2023-10-12 14:06:15 +0800

    /append

--- api.go
+++ api.go
@@ -299,7 +299,7 @@ func extractOk(data *bytes.Buffer) error {
 func extractMessage(data *bytes.Buffer) (*Message, error) {
 	defer ReleaseBuffer(data)
 	var resp Response[*Message]
-	if err := defaultJson.Unmarshal(data.Bytes(), &resp); err != nil {
+	if err := defaultJson.Unmarshal(data.Bytes(), resp); err != nil {
 		var resp Response[bool]
 		if err := defaultJson.NewDecoder(data).Decode(&resp); err != nil {
 			return nil, wrapError(err)
